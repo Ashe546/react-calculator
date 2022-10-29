37a57192aba2bf8c7e85dd1a8bf6cd93fe1e1da1
Ashe546 <asfyd42@gmail.com>
2022-10-29 11:01:47 -0300

    fixed according to reviewer

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,45 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
-  render() {
-    return (
-      <div className="center">
-        <h1>Calculator</h1>
-        <div className="Container">
-          <div className="display-name"><p>0</p></div>
-          <div className="row-1">
-            <div className="box"><p>AC</p></div>
-            <div className="box"><p>+/-</p></div>
-            <div className="box"><p>%</p></div>
-            <div className="box orange"><p>+</p></div>
-          </div>
-          <div className="row-1">
-            <div className="box"><p>7</p></div>
-            <div className="box"><p>8</p></div>
-            <div className="box"><p>9</p></div>
-            <div className="box orange"><p>*</p></div>
-          </div>
-          <div className="row-1">
-            <div className="box"><p>4</p></div>
-            <div className="box"><p>5</p></div>
-            <div className="box"><p>6</p></div>
-            <div className="box orange"><p>-</p></div>
-          </div>
-          <div className="row-1">
-            <div className="box"><p>1</p></div>
-            <div className="box"><p>2</p></div>
-            <div className="box"><p>3</p></div>
-            <div className="box orange"><p>+</p></div>
-          </div>
-          <div className="row-1">
-            <div className="box-50"><p>0</p></div>
-            <div className="box"><p>.</p></div>
-            <div className="box orange"><p>=</p></div>
-          </div>
-        </div>
+const Calculator = () => {
+  const [state, setState] = useState({
+    next: null,
+    total: '',
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    const pressedButton = e.target.innerText;
+    const newState = calculate(state, pressedButton);
+    setState(newState);
+  };
+
+  const { next, total, operation } = state;
+  return (
+
+    <div className="container">
+      <h1>Simple Calculator</h1>
+      <div className="display">
+        <span>{next}</span>
+        <span>{operation}</span>
+        <span>{total}</span>
       </div>
-    );
-  }
-}
+      <div>
+        <button value="AC" type="button" onClick={handleClick}>AC</button>
+        <button value="+/-" type="button" onClick={handleClick}>+/-</button>
+        <button value="%" type="button" onClick={handleClick}>%</button>
+        <button value="/" className="orange" onClick={handleClick} type="button"> &#247;</button>
+      </div>
+      <div>
+        <button value="7" type="button" onClick={handleClick}>7</button>
+        <button value="8" type="button" onClick={handleClick}>8</button>
+        <button value="9" type="button" onClick={handleClick}>9</button>
+        <button value="*" className="orange" onClick={handleClick} type="button">x</button>
+      </div>
+      <div>
+        <button value="4" type="button" onClick={handleClick}>4</button>
+        <button value="5" type="button" onClick={handleClick}>5</button>
+        <button value="6" type="button" onClick={handleClick}>6</button>
+        <button value="-" className="orange" onClick={handleClick} type="button">-</button>
+      </div>
+      <div>
+        <button value="1" type="button" onClick={handleClick}>1</button>
+        <button value="2" type="button" onClick={handleClick}>2</button>
+        <button value="3" type="button" onClick={handleClick}>3</button>
+        <button value="+" className="orange" onClick={handleClick} type="button">+</button>
+      </div>
+      <div>
+        <button value="0" onClick={handleClick} className="wide-button" type="button">0</button>
+        <button value="." type="button" onClick={handleClick}>.</button>
+        <button value="=" onClick={handleClick} className="orange" type="button">=</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
