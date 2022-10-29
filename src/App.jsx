0faa6893b6ba2ffa73bbe1d02ccd05b90f4f2956
Ashe546 <asfyd42@gmail.com>
2022-10-29 07:47:59 -0300

    Added event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
-import Calculator from './components/Calculator';
 import './App.css';
+import Calculator from './components/Calculator';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return <Calculator />;
 }
+
+export default App;
